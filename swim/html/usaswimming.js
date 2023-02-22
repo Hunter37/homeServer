baf45e4660dd2224deb1259002449bd9d1eefbea
Xuewei Han <hunterhan37@msn.com>
2023-02-22 12:36:28 -0800

    make usaswimming script running from browser menu

--- swim/html/usaswimming.js
+++ swim/html/usaswimming.js
@@ -1,5 +1,49 @@
 // https://hub.usaswimming.org/
 
+//let token = await getToken();
+//console.log(JSON.stringify(await findUsaSwimmer('Boyu Chen', '01/04/2011 - 01/04/2011', token), null, 4))
+
+document.body.innerHTML='<input id="name"><input id="bday"><button id="run">Search</button><div id="content"></div>';
+
+let contentElem = document.getElementById('content');
+let nameElem = document.getElementById('name');
+let bdayElem = document.getElementById('bday');
+
+nameElem.addEventListener('keypress', (event) => {
+    if (event.key == 'Enter') {
+        search();
+    }
+});
+
+bdayElem.addEventListener('keypress', (event) => {
+    if (event.key == 'Enter') {
+        search();
+    }
+});
+
+document.getElementById('run').addEventListener('click', () => {
+    search();
+});
+
+function log(str) {
+    contentElem.innerHTML += '<div>' + str + '</div>';
+}
+
+let token = null
+
+async function search() {
+    if (!token) {
+        token = await getToken();
+    }
+    contentElem.innerHTML = 'Searching...';
+    let data = await findUsaSwimmer(nameElem.value, bdayElem.value, token);
+    if (data) {
+        log('<pre>' + JSON.stringify(data, null, 4) + '</pre>');
+    } else {
+        log('Not exist');
+    }
+}
+
 async function getToken() {
     let resp = await fetch('https://securityapi.usaswimming.org/security/Account/Login', {
         method: 'POST',
@@ -28,9 +72,9 @@ async function usaSwimming(first, last, birthday, token) {
             headers: {
                 'Authorization': token
             }
-        })
+        });
 
-    return await response.json()
+    return await response.json();
 }
 
 async function findUsaSwimmer(name, date, token) {
@@ -38,32 +82,28 @@ async function findUsaSwimmer(name, date, token) {
     let first = name[0];
     let last = name[1];
     date = date.split(' - ');
-    let begin = date[0]
-    let end = date[1]
+    let begin = date[0];
+    let end = date[1];
     if (end.length < 6) {
-        end = begin.split('/')[0] + '/' + end
+        end = begin.split('/')[0] + '/' + end;
     }
     begin = new Date(begin);
     end = new Date(end);
     for (let day = begin; day.getTime() <= end.getTime(); day.setDate(day.getDate() + 1)) {
         let dateStr = day.toLocaleDateString('en-US');
-        console.log(dateStr);
+        log(dateStr);
         let result = await usaSwimming(first, last, day.toLocaleString('en-US'), token);
         if (result.length > 0) {
             console.log(result);
-            console.log('"Birthday":"' + result[0].birthDate.substring(0, 10).replaceAll('-', '/') + '",');
+            log('"Birthday":"' + result[0].birthDate.substring(0, 10).replaceAll('-', '/') + '",');
             if (result[0].preferredName.trim() && result[0].preferredName != result[0].firstName) {
-                console.log('"Alias":"' + result[0].preferredName + ' ' + result[0].lastName + '",');
+                log('"Alias":"' + result[0].preferredName + ' ' + result[0].lastName + '",');
             }
             if (result[0].middleName.trim()) {
-                console.log('"Middle":"' + result[0].middleName + '",')
+                log('"Middle":"' + result[0].middleName + '",');
             }
             return result;
         }
     }
     return null;
 }
-
-let token = await getToken()
-
-console.log(JSON.stringify(await findUsaSwimmer('Boyu Chen', '01/04/2011 - 01/04/2011', token), null, 4))
