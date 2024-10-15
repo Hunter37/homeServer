9f356f55b174e435bc43d47990846ab097bb5e37
REDMOND\xueweihan <xueweihan@microsoft.com>
2024-10-15 11:23:24 -0700

    minfy html file

--- file/files/min.ps1
+++ file/files/min.ps1
@@ -1,7 +1,9 @@
 javascript-obfuscator swim.js -o swim.min.js --config config.json
 
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true -o swim.min.html swim.html
+
 # Read the content of swim.html
-$htmlContent = Get-Content -Path "swim.html" -Raw
+$htmlContent = Get-Content -Path "swim.min.html" -Raw
 
 # Read the content of swim.min.js
 $minJsContent = Get-Content -Path "swim.min.js" -Raw
