2ef5bd5799651ec8abd1aabf9adfc8ffc337884d
REDMOND\xueweihan <xueweihan@microsoft.com>
2025-01-14 11:53:36 -0800

    fix style & min.ps1

--- file/files/min.ps1
+++ file/files/min.ps1
@@ -1,5 +1,24 @@
 Write-Output "Minifying swim.js..."
-javascript-obfuscator swim.js -o swim.min.js --config config.json
+
+$minJsContent = ""
+$minJsFileSize = [long]::MaxValue
+
+for ($i = 1; $i -le 10; $i++) {
+    javascript-obfuscator swim.js -o swim.min.js --config config.json
+
+    # Get the file size of the minified JS
+    $currentFileSize = (Get-Item -Path "swim.min.js").Length
+
+    # If the current file size is smaller, update the minJsContent and minJsFileSize
+    if ($currentFileSize -lt $minJsFileSize) {
+        $minJsFileSize = $currentFileSize
+        $minJsContent = Get-Content -Path "swim.min.js" -Raw
+    }
+}
+Write-Output "Using the smallest minified JS file with size $minJsFileSize bytes"
+
+# Remove the minified JS file
+Remove-Item -Path "swim.min.js"
 
 Write-Output "Merging swim.html and swim.css..."
 # Read the content of swim.html
@@ -21,9 +40,6 @@ Write-Output "Generating swim.min.html..."
 # Read the content of swim.html
 $htmlContent = Get-Content -Path "swim.min.html" -Raw
 
-# Read the content of swim.min.js
-$minJsContent = Get-Content -Path "swim.min.js" -Raw
-
 # Replace the script tag with the minified JS content
 $updatedHtmlContent = $htmlContent -replace '<script src="swim.js"></script>', "<script>$minJsContent</script>"
 
@@ -31,7 +47,5 @@ $updatedHtmlContent = $htmlContent -replace '<script src="swim.js"></script>', "
 Set-Content -Path "swim.min.html" -Value $updatedHtmlContent
 Set-Content -Path "swim1.min.html" -Value $updatedHtmlContent
 
-# Remove the minified JS file
-Remove-Item -Path "swim.min.js"
 
 ls swim.*
